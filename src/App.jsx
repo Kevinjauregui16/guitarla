bacbf3cb072e881337b34e8f20e7f3f52ac928be
kevinjg16 <desarrollo@solar-center.mx>
2024-06-12 17:54:55 -0600

    quantityItem

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,20 @@ import Header from "./components/Header";
 import { db } from "./data/db";
 
 function App() {
-  
-  const [data, setData] = useState(db)
-  const [cart, setCart] = useState([])
+  const [data, setData] = useState(db);
+  const [cart, setCart] = useState([]);
+
+  function addToCart(item) {
+    const itemExists = cart.findIndex((guitar) => guitar.id === item.id);
+    if (itemExists >= 0) {
+      const updatedCart = [...cart];
+      updatedCart[itemExists].quantity++;
+      setCart(updatedCart);
+    } else {
+      item.quantity = 1;
+      setCart([...cart, item]);
+    }
+  }
 
   return (
     <>
@@ -16,12 +27,13 @@ function App() {
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-          
           {data.map((guitar) => (
-            <Guitar 
-            key={guitar.id}
-            guitar={guitar} 
-            setCart={setCart}/>
+            <Guitar
+              key={guitar.id}
+              guitar={guitar}
+              setCart={setCart}
+              addToCart={addToCart}
+            />
           ))}
         </div>
       </main>
--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,9 +1,5 @@
-export default function Guitar({ guitar, setCart }) {
+export default function Guitar({ guitar, addToCart }) {
   const { id, name, image, description, price } = guitar;
-
-  const handleClick = (id) => {
-    console.log('diste click', id)
-  };
 
   return (
     <div className="col-md-6 col-lg-4 my-4 row align-items-center">
@@ -21,7 +17,7 @@ export default function Guitar({ guitar, setCart }) {
         <button
           type="button"
           className="btn btn-dark w-100 "
-          onClick={() => handleClick(guitar)}
+          onClick={() => addToCart(guitar)}
         >
           Agregar al Carrito
         </button>
